47bf9650e758c1047dc201559300f02972c8d455
SeanM13439 <43637382+SeanM13439@users.noreply.github.com>
2018-12-11 15:38:54 +0000

    Hosting Website
    Hosting Website

---
+++ index3.js
@@ -0,0 +1,30 @@
+var accordions = document.getElementsByClassName("workxpaccordion");
+
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function () {
+      
+        var content = this.nextElementSibling;
+        if(content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
